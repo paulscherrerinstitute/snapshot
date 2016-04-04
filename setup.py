6bc76ceb11a321c0c00cd5189c91c14c043ea72f
Vintar Rok <rok.vintar@psi.ch>
2016-04-04 08:01:45 +0200

    Version 1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='snapshot',
-      version='1.1.1',
+      version='1.1.2',
       description="Tool for saving and restoring snapshots of EPICS channels",
       long_description=README,
       author='Rok Vintar',
