85a766877b990bf0295f9638751e6fd889654842
Simon Ebner <simon.ebner@psi.ch>
2020-05-12 13:23:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='snapshot',
-      version='2.0.23',
+      version='2.0.25',
       description="Tool for saving and restoring snapshots of EPICS channels",
       long_description=README,
       author='Paul Scherrer Institute',
